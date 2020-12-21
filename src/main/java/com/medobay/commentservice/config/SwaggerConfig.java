8c809b67d3dc18d80fc051159d71d1b27f67cd17
mohammad farahani <mfarahani31@gmail.com>
2020-12-21 20:32:45 +0330

    add Swagger

--- src/main/java/com/medobay/commentservice/config/SwaggerConfig.java
+++ src/main/java/com/medobay/commentservice/config/SwaggerConfig.java
@@ -1,58 +1,51 @@
-//package com.gam.phoenix.numberingformat.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.ParameterBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.schema.ModelRef;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//import java.util.Collections;
-//
-///**
-// * @author Mohammad Farahani (farahani@gamelectronics.com)
-// **/
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig implements WebMvcConfigurer {
-//    @Bean
-//    public Docket apiDocket() {
-//        return (new Docket(DocumentationType.SWAGGER_2))
-//                .globalOperationParameters(
-//                        Collections.singletonList(
-//                                new ParameterBuilder()
-//                                        .name("USERNAME")
-//                                        .modelRef(new ModelRef("string"))
-//                                        .parameterType("header")
-//                                        .required(false)
-//                                        .build()))
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.regex("(?!/error).+"))
-//                .paths(PathSelectors.regex("(?!/actuator).+"))
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-//
-//
-//    @Override
-//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//        configurer.enable();
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder().title("Numbering-Format-Service")
-//                .description("Numbering-Format-Service")
-//                .license("Apache 2.0")
-//                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-//                .version("0.5.1")
-//                .build();
-//    }
-//}
+package com.medobay.commentservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+/**
+ * @author Mohammad Farahani (mfarahani31@gmail.com)
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Bean
+    public Docket apiDocket() {
+        return (new Docket(DocumentationType.SWAGGER_2))
+                .globalOperationParameters(
+                        Collections.singletonList(
+                                new ParameterBuilder()
+                                        .name("USERNAME")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
+                                        .build()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Comment-Service")
+                .description("Comment-Service")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+}
